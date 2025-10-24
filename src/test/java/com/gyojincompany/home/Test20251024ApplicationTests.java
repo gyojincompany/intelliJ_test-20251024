0d44977667c56f20993098cf44d027f7f4f7203f
gyojincompany <luckyred@gmail.com>
2025-10-24 11:39:14 +0900

    기본 프로젝트 설정

---
+++ src/test/java/com/gyojincompany/home/Test20251024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test20251024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
